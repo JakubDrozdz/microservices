c3a3cae1180d6ae764e460944c93a70147c8b319
Jakub Drozdz <jakubekso@outlook.com>
2023-06-12 17:08:13 +0200

    Create first microservice, adds docker compose

---
+++ customer/src/main/java/pl/jakubdrozdz/customer/CustomerRegistrationRequest.java
@@ -0,0 +1,7 @@
+package pl.jakubdrozdz.customer;
+
+public record CustomerRegistrationRequest(
+        String firstName,
+        String lastName,
+        String email) {
+}
---
+++ customer/src/main/java/pl/jakubdrozdz/customer/CustomerService.java
@@ -0,0 +1,17 @@
+package pl.jakubdrozdz.customer;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public record CustomerService() {
+    public void registerCustomer(CustomerRegistrationRequest customerRegistrationRequest) {
+        Customer customer = Customer.builder()
+                .firstName(customerRegistrationRequest.firstName())
+                .lastName(customerRegistrationRequest.lastName())
+                .email(customerRegistrationRequest.email())
+                .build();
+        //todo: validate email
+        //todo: check if email not used
+        //todo: store in db
+    }
+}
