2b08d6a4f2a5be0adcf486bee6f333c670098ed7
Jakub Drozdz <jakubekso@outlook.com>
2023-08-09 22:07:39 +0200

    implements reading from application properties

--- notification/src/main/java/pl/jakubdrozdz/notification/NotificationController.java
+++ notification/src/main/java/pl/jakubdrozdz/notification/NotificationController.java
@@ -1,6 +1,5 @@
 package pl.jakubdrozdz.notification;
 
-import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
--- notification/src/main/java/pl/jakubdrozdz/notification/NotificationService.java
+++ notification/src/main/java/pl/jakubdrozdz/notification/NotificationService.java
@@ -1,19 +1,28 @@
 package pl.jakubdrozdz.notification;
 
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import pl.jakubdrozdz.clients.notification.NotificationRequest;
 
 import java.time.LocalDateTime;
 
 @Service
-public record NotificationService(NotificationRepository notificationRepository) {
+public class NotificationService {
+    private final NotificationRepository notificationRepository;
+
+    @Value("${sender.mail}")
+    private String senderMail;
+
+    public NotificationService(NotificationRepository notificationRepository) {
+        this.notificationRepository = notificationRepository;
+    }
+
     public void send(NotificationRequest notificationRequest) {
         notificationRepository.save(
                 Notification.builder()
                         .toCustomerId(notificationRequest.toCustomerId())
                         .toCustomerEmail(notificationRequest.toCustomerEmail())
-                        .sender("test@test.test")
+                        .sender(senderMail)
                         .message(notificationRequest.message())
                         .sentAt(LocalDateTime.now())
                         .build()
