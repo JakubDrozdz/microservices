cdf7d9270e31a417c481f77f390dd0d6b980e4f9
Jakub Drozdz <jakubekso@outlook.com>
2023-08-24 16:22:50 +0200

    fix for executing api request in swagger ui

--- notification/src/main/java/pl/jakubdrozdz/notification/NotificationController.java
+++ notification/src/main/java/pl/jakubdrozdz/notification/NotificationController.java
@@ -1,15 +1,13 @@
 package pl.jakubdrozdz.notification;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pl.jakubdrozdz.clients.notification.NotificationRequest;
 
 @RestController
 @RequestMapping("api/v1/notification")
 @Slf4j
+@CrossOrigin(origins = "http://localhost")
 public record NotificationController(NotificationService notificationService) {
     @PostMapping
     public void sendNotification(@RequestBody NotificationRequest notificationRequest) {
